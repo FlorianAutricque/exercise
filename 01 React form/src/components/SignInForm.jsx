9924c62b0fc2d05f273dcf1dfb06f301c901aa05
florian autricque <fautricque@gmail.com>
2023-10-13 20:28:45 -0400

    welcome message

--- 01 React form/src/components/SignInForm.jsx
+++ 01 React form/src/components/SignInForm.jsx
@@ -1,22 +1,39 @@
 import { Link } from "react-router-dom";
 import styles from "./Form.module.css";
+import { useState } from "react";
 
 function SignInForm() {
+  const [show, setShow] = useState(false);
+
+  function handleShow(e) {
+    e.preventDefault();
+    setShow(show => !show);
+  }
+
   return (
     <div className={styles.mainContainer}>
-      <form className={styles.formContainer}>
-        <label htmlFor="email">Your Email</label>
-        <input type="email" required />
+      {show ? (
+        <>
+          <p>Welcome!</p>
+          <button onClick={handleShow}>Go back</button>
+        </>
+      ) : (
+        <>
+          <form className={styles.formContainer} onSubmit={handleShow}>
+            <label htmlFor="email">Your Email</label>
+            <input type="email" required />
 
-        <label htmlFor="password">Password</label>
-        <input type="password" required />
+            <label htmlFor="password">Password</label>
+            <input type="password" required />
 
-        <button type="submit">Sign In</button>
-      </form>
-      <span>
-        <p>Not registered yet ?</p>
-        <Link to="/signup"> Sign up </Link>
-      </span>
+            <button type="submit">Sign In</button>
+          </form>
+          <span>
+            <p>Not registered yet ?</p>
+            <Link to="/signup"> Sign up </Link>
+          </span>
+        </>
+      )}
     </div>
   );
 }
