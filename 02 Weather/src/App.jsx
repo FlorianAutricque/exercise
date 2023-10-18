b46e77092394722a728392361c7954e0267db581
florian autricque <fautricque@gmail.com>
2023-10-18 15:37:47 -0400

    fetching data

--- 02 Weather/src/App.jsx
+++ 02 Weather/src/App.jsx
@@ -1,25 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [isLoading, setIsloading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [location, setLocation] = useState("");
+  const [weather, setWeather] = useState({});
 
-  // async function fetch() {
-  //   try {
-  //     setIsloading(true);
-  //     const res = await fetch(
-  //       `https://geocoding-api.open-meteo.com/v1/search?name=${location}&count=10&language=en&format=json`
-  //     );
-  //     const data = res.json();
-  //     console.log(data);
-  //   } catch (error) {
-  //     console.error(error);
-  //   } finally {
-  //     setIsloading(false);
-  //   }
-  // }
-  // fetch();
-  console.log(location);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        if (location.length >= 4) {
+          const res = await fetch(
+            `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
+          );
+          const data = await res.json();
+
+          if (!data.results) throw new Error("Location not found");
+
+          const { latitude, longitude, timezone } = data.results.at(0);
+
+          const weatherRes = await fetch(
+            `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=${timezone}`
+          );
+
+          const weatherData = await weatherRes.json();
+
+          setWeather(weatherData.daily);
+          console.log(weatherData);
+        } else {
+          setWeather({});
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchData();
+  }, [location]);
 
   return (
     <div>
@@ -29,6 +48,12 @@ function App() {
         value={location}
         onChange={e => setLocation(e.target.value)}
       />
+
+      {isLoading && <p>Loading...</p>}
+
+      {weather && weather.temperature_2m_max && (
+        <p>Max Temperature: {Math.max(...weather.temperature_2m_max)}°C</p>
+      )}
     </div>
   );
 }
