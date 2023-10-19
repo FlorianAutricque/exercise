36700c0f0ba288f6dbc84a5ab909a157383bfcf2
florian autricque <fautricque@gmail.com>
2023-10-19 12:45:31 -0400

    key

--- 02 Weather/src/App.jsx
+++ 02 Weather/src/App.jsx
@@ -1,78 +1,48 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
-function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [location, setLocation] = useState("");
-  const [weather, setWeather] = useState({});
+function CityImages() {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const accessKey = import.meta.env.VITE_API_KEY;
+  // const accessKey = "ynmV5qtX3xGG1RKS8136bMIme_5pMEMwRVAyVpuBp-g";
+  console.log(accessKey);
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        setIsLoading(true);
-        if (location.length >= 4) {
-          const res = await fetch(
-            `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
-          );
-          const data = await res.json();
+    fetchImages();
+  }, []);
 
-          if (!data.results) throw new Error("Location not found");
+  const fetchImages = () => {
+    const query = "montreal";
+    const apiUrl = `https://api.unsplash.com/search/photos?query=${query}`;
 
-          const { latitude, longitude, timezone } = data.results.at(0);
-
-          const weatherRes = await fetch(
-            `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=${timezone}`
-          );
-
-          //use google places api to fetch image from location
-
-          const weatherData = await weatherRes.json();
-
-          setWeather(weatherData.daily);
-          console.log(weatherData);
-        } else {
-          setWeather({});
-        }
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchData();
-  }, [location]);
-
-  // const { time, temperature_2m_max, temperature_2m_min, weathercode } = weather;
+    fetch(apiUrl, {
+      headers: {
+        Authorization: `Client-ID ${accessKey}`,
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        setImages(data.results);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error("Error fetching images:", error);
+        setLoading(false);
+      });
+  };
 
   return (
     <div>
-      <h1>Weather {location}</h1>
-      <input
-        type="text"
-        value={location}
-        onChange={e => setLocation(e.target.value)}
-      />
-
-      {isLoading && <p>Loading...</p>}
-
-      {weather &&
-        weather.temperature_2m_max &&
-        weather.weathercode &&
-        weather.temperature_2m_min &&
-        weather.temperature_2m_max.map((max, index) => (
-          <div key={index}>
-            <p>
-              Max Temperature for {weather.time[index]}: {max}°C
-            </p>
-            <p>
-              WC:
-              {weather.weathercode[index]}
-            </p>
-            <p>min: {weather.temperature_2m_min[index]}C</p>
-          </div>
-        ))}
+      <h1>City Images</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : images.length > 0 ? (
+        <img src={images[0].urls.small} alt={images[0].description} />
+      ) : (
+        <p>No images found.</p>
+      )}
     </div>
   );
 }
 
-export default App;
+export default CityImages;
