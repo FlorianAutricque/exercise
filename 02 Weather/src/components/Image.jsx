7cf8bb31511bc064ded8cab291d53fac93d350ff
florian autricque <fautricque@gmail.com>
2023-10-20 14:20:10 -0400

    adding more style

--- 02 Weather/src/components/Image.jsx
+++ 02 Weather/src/components/Image.jsx
@@ -6,7 +6,7 @@ function Image({ isLoading, images }) {
       ) : images.length > 0 ? (
         <img src={images[0].urls.regular} alt={images[0].description} />
       ) : (
-        <p>No images found.</p>
+        ""
       )}
     </>
   );
