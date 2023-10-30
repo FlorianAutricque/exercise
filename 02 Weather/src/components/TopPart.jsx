fb036d0704455c604912592b4b4a85ae127d9e21
florian autricque <fautricque@gmail.com>
2023-10-29 21:45:58 -0400

    fixig bug

--- 02 Weather/src/components/TopPart.jsx
+++ 02 Weather/src/components/TopPart.jsx
@@ -4,14 +4,14 @@ import Title from "./Title";
 
 import styles from "./TopPart.module.css";
 
-function TopPart({ weather, location }) {
+function TopPart({ weather, location, setLocation }) {
   return (
     <div className={styles.mainContainerTop}>
       <div className={styles.topContainer}>
         <Infos weather={weather} />
         <div>
           <Title location={location} />
-          <Input location={location} />
+          <Input location={location} setLocation={setLocation} />
         </div>
       </div>
     </div>
