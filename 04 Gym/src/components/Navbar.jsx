a6476e00cc33c9db4021b8c148df6f624be3ec02
florian autricque <fautricque@gmail.com>
2024-01-27 16:39:24 -0500

    navbar styling

--- 04 Gym/src/components/Navbar.jsx
+++ 04 Gym/src/components/Navbar.jsx
@@ -1,21 +1,34 @@
+import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import { HashLink } from "react-router-hash-link";
 
 function Navbar() {
+  function scrollTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
   return (
-    <div>
-      <HashLink smooth to="#welcome">
-        <p>Welcome</p>
-      </HashLink>
-      <HashLink smooth to="#feedbacks">
-        <p>Feedbacks</p>
-      </HashLink>
-      <HashLink smooth to="#prices">
-        <p>Prices</p>
-      </HashLink>
-      <HashLink smooth to="#offers">
-        <p>Offers</p>
-      </HashLink>
+    <div className={styles.containerNavbar}>
+      <Link onClick={scrollTop} className={styles.logo}>
+        GymRat
+      </Link>
+      <div className={styles.containerLinks}>
+        <HashLink smooth to="#welcome">
+          <p>Welcome</p>
+        </HashLink>
+        <HashLink smooth to="#feedbacks">
+          <p>Feedbacks</p>
+        </HashLink>
+        <HashLink smooth to="#prices">
+          <p>Prices</p>
+        </HashLink>
+        <HashLink smooth to="#offers">
+          <p>Offers</p>
+        </HashLink>
+      </div>
     </div>
   );
 }
--- 04 Gym/src/components/section1/Welcome.jsx
+++ 04 Gym/src/components/section1/Welcome.jsx
@@ -1,6 +1,59 @@
+import styles from "./Welcome.module.css";
+
 function Welcome() {
   return (
-    <div id="welcome">
+    <div id="welcome" className={styles.mainContainer}>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
+      <p>Welcome message here</p>
       <p>Welcome message here</p>
     </div>
   );
