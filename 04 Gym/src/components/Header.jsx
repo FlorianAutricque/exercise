ada7906bd7c11f9d21ea280de44438055bcb3578
florian autricque <fautricque@gmail.com>
2024-01-30 17:45:26 -0500

    working on header

--- 04 Gym/src/components/Header.jsx
+++ 04 Gym/src/components/Header.jsx
@@ -3,9 +3,20 @@ import styles from "./Header.module.css";
 
 function Header() {
   return (
-    <div>
+    <div className={styles.mainContainerHeader}>
       <div className={styles.imageContainer}>
         <img src={headerImage} alt="header" className={styles.imageHeader} />
+      </div>
+      <div className={styles.textHeader}>
+        <h2>
+          You Only Fail, <br /> If You Stop
+          <br /> Trying
+        </h2>
+        <p>
+          jvnrei fweeinfewiun <br />
+          fweoejnfwein
+          <br /> woieijefnewoifn
+        </p>
       </div>
     </div>
   );
