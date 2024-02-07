bcb86e04a27140cb8b25915f195be59b618ca18f
florian autricque <fautricque@gmail.com>
2024-02-06 21:02:19 -0500

    prices

--- 04 Gym/src/components/section3/Prices.jsx
+++ 04 Gym/src/components/section3/Prices.jsx
@@ -1,7 +1,27 @@
+import styles from "./Prices.module.css";
+import TitleNumber from "../../components/TitleNumber.jsx";
+
 function Prices() {
   return (
-    <div id="prices">
-      <p>Prices here</p>
+    <div id="prices" className={styles.mainContainerPrices}>
+      <TitleNumber
+        title={"PRICES TABLES"}
+        message={"Membership Plans"}
+        number={"03"}
+      />
+
+      <div>
+        <p>
+          $<span>4</span>/day
+        </p>
+
+        <p>One Day Training</p>
+
+        <p>Access All Equipments</p>
+        <p>Access Spa</p>
+        <p>Access Group Class</p>
+        <p>Only During Staff Hours</p>
+      </div>
     </div>
   );
 }
