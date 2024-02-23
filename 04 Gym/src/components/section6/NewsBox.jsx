ead92b7ed8795a691ba1f762accef3a4184215a7
florian autricque <fautricque@gmail.com>
2024-02-23 13:37:05 -0500

    styling

--- 04 Gym/src/components/section6/NewsBox.jsx
+++ 04 Gym/src/components/section6/NewsBox.jsx
@@ -1,5 +1,5 @@
 import { AiFillLike } from "react-icons/ai";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styles from "./NewsBox.module.css";
 
 function NewsBox({
