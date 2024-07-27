05aaa7677935e53f6c17fe9a4afea7887500c6f4
florian autricque <fautricque@gmail.com>
2024-07-27 16:33:07 -0400

    form styling and first js

--- 05 Form_training_interview/js/script.js
+++ 05 Form_training_interview/js/script.js
@@ -3,24 +3,66 @@ document.addEventListener("DOMContentLoaded", () => {
   const returnBtn = document.getElementById("returnBtn");
   const mainContainer = document.getElementById("mainContainer");
   const successContainer = document.getElementById("successContainer");
-  const input = document.getElementById("input");
+  const email = document.getElementById("email");
+  const inputs = document.querySelectorAll("input");
+  const textarea = document.querySelector("textarea");
+  const phone = document.getElementById("phone");
+
+  const errorMessage = document.getElementById("errorMessage");
+  const errorMessageEmail = document.getElementById("errorMessageEmail");
+  const errorMessageTextarea = document.getElementById("errorMessageTextarea");
+  const errorMessagePhone = document.getElementById("errorMessagePhone");
+
+  const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
+  const regexPhone = /^\d+$/;
 
   function handleSubmit(e) {
     e.preventDefault();
-    if (input.value == "") {
-      alert("error");
+
+    let fields = true;
+    inputs.forEach(input => {
+      if (input.value === "") {
+        fields = false;
+      }
+    });
+
+    if (!fields) {
+      errorMessage.style.display = "block";
+      return false;
+    }
+
+    if (!regexEmail.test(email.value)) {
+      errorMessageEmail.style.display = "block";
+      return false;
+    }
+
+    if (!regexPhone.test(phone.value)) {
+      errorMessagePhone.style.display = "block";
+      return false;
+    }
+
+    if (textarea.value.length <= 10) {
+      errorMessageTextarea.style.display = "block";
       return false;
     }
 
     mainContainer.style.display = "none";
     successContainer.style.display = "block";
   }
+
   submitBtn.addEventListener("click", handleSubmit);
 
   function handleReturn(e) {
     e.preventDefault();
     mainContainer.style.display = "block";
     successContainer.style.display = "none";
+    errorMessage.style.display = "none";
+    errorMessageEmail.style.display = "none";
+    errorMessageTextarea.style.display = "none";
+    errorMessagePhone.style.display = "none";
+    inputs.forEach(input => (input.value = ""));
+    textarea.value = "";
   }
+
   returnBtn.addEventListener("click", handleReturn);
 });
