2e247240c585ba434556d0df5cf112d6250fe8dc
florian autricque <fautricque@gmail.com>
2024-07-30 21:30:09 -0400

    todolist

--- 07 to_do_list/js/script.js
+++ 07 to_do_list/js/script.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // SHOW MORE text
   const originalText = text.textContent;
   const truncatedText = truncate(text.textContent, 40);
+  let tasks = [];
 
   function truncate(str, maxLength) {
     if (str.length >= maxLength) {
@@ -33,29 +34,63 @@ document.addEventListener("DOMContentLoaded", () => {
   btnShowMore.addEventListener("click", handleShow);
 
   // TODO LIST
-  function handleNewTask() {
-    const task = inputField.value;
 
-    //trim to delete whitespace
-    if (task.trim() !== "") {
-      //NEW EL
-      const listItem = document.createElement("li");
-      listItem.textContent = task;
-      //DELETE BTN
-      const deleteTask = document.createElement("button");
-      deleteTask.innerHTML = "X";
+  function saveToLocalStorage() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
 
-      deleteTask.addEventListener("click", function () {
-        newTask.removeChild(listItem);
-      });
-
-      //appendChild to add dynamically an el to the web page
-      listItem.appendChild(deleteTask);
+  function getFromLocalStorage() {
+    const storedTasks = localStorage.getItem("tasks");
+    tasks = storedTasks ? JSON.parse(storedTasks) : [];
+    tasks.forEach(taskText => {
+      const listItem = createElement(taskText);
       newTask.appendChild(listItem);
+    });
+  }
 
+  function createElement(task) {
+    //NEW EL
+    const listItem = document.createElement("li");
+
+    //CHECKBOX
+    const markDone = document.createElement("input");
+    markDone.type = "checkbox";
+    markDone.checked = task.completed;
+    markDone.addEventListener("change", function () {
+      task.completed = markDone.checked;
+      saveToLocalStorage();
+    });
+
+    //DELETE TASK
+    const removeTask = document.createElement("button");
+    removeTask.innerHTML = "X";
+    removeTask.addEventListener("click", function () {
+      newTask.removeChild(listItem);
+      tasks = tasks.filter(t => t.text != task.text);
+      saveToLocalStorage();
+    });
+
+    //APPEND
+    //appendChild to add dynamically an el to the web page
+    listItem.textContent = task.text;
+    listItem.appendChild(markDone);
+    listItem.appendChild(removeTask);
+
+    return listItem;
+  }
+
+  function handleTask() {
+    const taskText = inputField.value.trim();
+    if (taskText !== "") {
+      const task = { text: taskText, completed: false };
+      const listItem = createElement(task);
+      tasks.push(task);
+      newTask.appendChild(listItem);
+      saveToLocalStorage();
       inputField.value = "";
     }
   }
 
-  btnSubmitInput.addEventListener("click", handleNewTask);
+  getFromLocalStorage();
+  btnSubmitInput.addEventListener("click", handleTask);
 });
