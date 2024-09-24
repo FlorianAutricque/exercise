acb681c73d65a49000f2be463b92b9f7fb13cb0b
florian autricque <fautricque@gmail.com>
2024-09-24 19:19:12 +0200

    game done

--- 11-test/src/components/TicTacToe copy.jsx
+++ 11-test/src/components/TicTacToe copy.jsx
@@ -1,23 +1,21 @@
 import { useState } from "react";
 
 function TicTacToe() {
-  // const [data, setData] = useState(["", "", "", "", "", "", "", "", ""]);
-  const [data, setData] = useState(new Array(9).fill(""));
-
   const [count, setCount] = useState(0);
   const [player, setPlayer] = useState("X");
+  const [data, setData] = useState(["", "", "", "", "", "", "", "", ""]);
 
   const handleClick = num => {
     const newData = [...data];
     newData[num] = count % 2 === 0 ? "X" : "O";
-
     count % 2 === 0 ? setPlayer("O") : setPlayer("X");
-    setCount(count + 1);
+
     setData(newData);
+    setCount(count + 1);
   };
 
-  const calculateWinner = data => {
-    const lines = [
+  const calculateWinner = () => {
+    const combs = [
       [0, 1, 2],
       [3, 4, 5],
       [6, 7, 8],
@@ -28,8 +26,8 @@ function TicTacToe() {
       [2, 4, 6],
     ];
 
-    for (let index = 0; index < lines.length; index++) {
-      const [a, b, c] = lines[index];
+    for (let index = 0; index < combs.length; index++) {
+      const [a, b, c] = combs[index];
       if (data[a] && data[a] === data[b] && data[a] === data[c]) {
         return data[a];
       }
@@ -37,114 +35,97 @@ function TicTacToe() {
     return null;
   };
 
+  const winner = calculateWinner(data);
+  let status;
+  if (winner) {
+    status = "Winner is: " + winner;
+  }
+
   const reset = () => {
     setData(["", "", "", "", "", "", "", "", ""]);
     setCount(0);
   };
 
-  let winner;
-  winner = calculateWinner(data);
-
-  const [x, setX] = useState([0, 1, 2]);
-
   return (
     <div>
-      <div className="mainContainerGame">
-        <h1>Tic Tac Toe Game</h1>
+      <h1>Tic Tac Toe game</h1>
 
+      <div className="mainContainerGame">
         {!winner ? (
           <p>
-            Turn: <span>{player}</span>&apos;s turn
+            Player: <span>{player}</span>&apos;s turn
           </p>
         ) : (
-          ""
+          <p>Press Reset below to start over</p>
         )}
 
-        {winner ? (
-          <p>
-            Congratulations to <span>{winner}</span>, you won the game!
-          </p>
-        ) : (
-          ""
-        )}
-
+        <p>{status}</p>
         <div className="containerGame">
           <div className="row1">
-            {x.map((x, i) => (
-              <button
-                key={i}
-                className="box"
-                onClick={() => handleClick(x)}
-                disabled={data[x] !== ""}
-              >
-                {data[x]}
-              </button>
-            ))}
-
-            {/* <button
+            <button
+              disabled={data[0] !== ""}
               className="box"
               onClick={() => handleClick(0)}
-              disabled={data[0] !== ""}
             >
               {data[0]}
             </button>
             <button
+              disabled={data[1] !== ""}
               className="box"
               onClick={() => handleClick(1)}
-              disabled={data[1] !== ""}
             >
               {data[1]}
             </button>
             <button
+              disabled={data[2] !== ""}
               className="box"
               onClick={() => handleClick(2)}
-              disabled={data[2] !== ""}
             >
               {data[2]}
-            </button> */}
+            </button>
           </div>
           <div className="row2">
             <button
+              disabled={data[3] !== ""}
               className="box"
               onClick={() => handleClick(3)}
-              disabled={data[3] !== ""}
             >
               {data[3]}
             </button>
             <button
+              disabled={data[4] !== ""}
               className="box"
               onClick={() => handleClick(4)}
-              disabled={data[4] !== ""}
             >
               {data[4]}
             </button>
             <button
+              disabled={data[5] !== ""}
               className="box"
               onClick={() => handleClick(5)}
-              disabled={data[5] !== ""}
             >
               {data[5]}
             </button>
           </div>
           <div className="row3">
             <button
+              disabled={data[6] !== ""}
               className="box"
               onClick={() => handleClick(6)}
-              disabled={data[6] !== ""}
             >
               {data[6]}
             </button>
             <button
+              disabled={data[7] !== ""}
               className="box"
               onClick={() => handleClick(7)}
-              disabled={data[7] !== ""}
             >
               {data[7]}
             </button>
             <button
+              disabled={data[8] !== ""}
               className="box"
               onClick={() => handleClick(8)}
-              disabled={data[8] !== ""}
             >
               {data[8]}
             </button>
