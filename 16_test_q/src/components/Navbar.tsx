727847a839ffe87b6af4ffab2e69f25f92a733b4
florian autricque <fautricque@gmail.com>
2024-10-17 19:16:22 +0200

    new features

--- 16_test_q/src/components/Navbar.tsx
+++ 16_test_q/src/components/Navbar.tsx
@@ -1,56 +1,35 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
-import { Spin as Hamburger } from "hamburger-react";
-import { useState } from "react";
 
 function Navbar() {
-  const [show, setShow] = useState<boolean>(false);
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const handleClick = () => {
-    setShow(!show);
-    setIsOpen(!isOpen);
-  };
-
   return (
     <nav className={styles.mainContainerNavbar}>
-      <NavLink to="/">
-        <div>LOGO</div>
-      </NavLink>
-
-      <ul className={`${styles.containerLinks} ${show ? styles.showMenu : ""}`}>
+      <ul className={styles.containerLinks}>
         <li>
           <NavLink
             to="/"
-            onClick={handleClick}
             className={({ isActive }) => (isActive ? styles.active : "")}
           >
             Home
           </NavLink>
         </li>
-        <li>
+        <li className={styles.linkBorder}>
           <NavLink
             to="/completed"
-            onClick={handleClick}
             className={({ isActive }) => (isActive ? styles.active : "")}
           >
-            Completed
+            Done
           </NavLink>
         </li>
         <li>
           <NavLink
             to="/not-completed"
-            onClick={handleClick}
             className={({ isActive }) => (isActive ? styles.active : "")}
           >
-            Not completed
+            Not Done
           </NavLink>
         </li>
       </ul>
-
-      <div className={styles.hamburger} onClick={handleClick}>
-        <Hamburger toggled={isOpen} toggle={setIsOpen} />
-      </div>
     </nav>
   );
 }
